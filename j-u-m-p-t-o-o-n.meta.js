c8222883a189844f701e137419c727389b011e58
kikuchiryo1234 <kikuchiryo1234@github.com>
2024-10-12 13:12:15 +0700

    init

---
+++ j-u-m-p-t-o-o-n.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         j-u-m-p-t-o-o-n
+// @namespace    j-u-m-p-t-o-o-n.user.js
+// @version      2024.10.12
+// @description  down j-u-m-p-t-o-o-n
+// @author       You
+// @include      /^https:\/\/j[u2]mpt[o0]on\.c[o0]m\/s[e3]ri[e3]s\/.+?\/[e3]pis[o0]des\//
+// @icon         https://raw.githubusercontent.com/kikuchiryo1234/va_ngon_lau/main/j-u-m-p-t-o-o-n.png
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant        GM_addElement
+// @downloadURL  https://github.com/kikuchiryo1234/va_ngon_lau/raw/main/j-u-m-p-t-o-o-n.user.js
+// @updateURL    https://github.com/kikuchiryo1234/va_ngon_lau/raw/main/j-u-m-p-t-o-o-n.meta.js
+// ==/UserScript==
